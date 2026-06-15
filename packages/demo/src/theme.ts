5f79698c113d5dacb0299deb42a7555fe9fdb7ca
guoshencheng <648772021@qq.com>
2026-06-15 09:31:17 +0800

    feat(demo): add markdown theme and message styles

---
+++ packages/demo/src/theme.ts
@@ -0,0 +1,31 @@
+import type { MarkdownTheme, DefaultTextStyle } from "@earendil-works/pi-tui";
+import { bold, dim, blue, yellow, cyan, italic, underline, strikethrough } from "./colors.js";
+
+export const markdownTheme: MarkdownTheme = {
+  heading: bold,
+  link: blue,
+  linkUrl: dim,
+  code: yellow,
+  codeBlock: yellow,
+  codeBlockBorder: dim,
+  quote: italic,
+  quoteBorder: dim,
+  hr: dim,
+  listBullet: cyan,
+  bold: bold,
+  italic: italic,
+  strikethrough: strikethrough,
+  underline: underline,
+};
+
+export const userMessageStyle: DefaultTextStyle = {
+  bgColor: (text) => `\x1b[48;5;236m${text}\x1b[0m`,
+};
+
+export const assistantMessageStyle: DefaultTextStyle = {
+  color: (text) => text,
+};
+
+export const eventLogStyle: DefaultTextStyle = {
+  color: dim,
+};

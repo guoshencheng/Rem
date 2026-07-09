959a4431e8927402087a69a28cf63876d73b5fbf
guoshencheng <648772021@qq.com>
2026-07-09 21:28:12 +0800

    refactor(tui): remove TUI package and related files

--- packages/tui/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { TUIApp } from "./app.js";
-export type { TUIAppOptions } from "./app.js";
-export { buildUI } from "./ui-layout.js";
-export type { UILayout } from "./ui-layout.js";
-export { showPicker, hidePicker, switchSession } from "./session-picker.js";
-export { handleNewSession, handleResumeCommand, generateId } from "./commands.js";

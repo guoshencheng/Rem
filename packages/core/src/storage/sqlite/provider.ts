ff14c07ee0338bde8e02587a253408b6625c1114
guoshencheng <648772021@qq.com>
2026-07-13 11:26:13 +0800

    feat(storage): expose SqliteTodoStore from SqliteStorageProvider

--- packages/core/src/storage/sqlite/provider.ts
+++ packages/core/src/storage/sqlite/provider.ts
@@ -5,6 +5,7 @@ import type { StorageProvider } from '../types.js';
 import { SqliteSchemaManager } from '../schema.js';
 import { SqliteSessionStore } from './session-store.js';
 import { SqliteRuleStore } from './rule-store.js';
+import { SqliteTodoStore } from './todo-store.js';
 import { StorageError, wrapSqliteError } from '../errors.js';
 
 export interface SqliteStorageProviderOptions {
@@ -15,6 +16,7 @@ export class SqliteStorageProvider implements StorageProvider {
   private db: Database.Database | undefined;
   private _sessionStore: SqliteSessionStore | undefined;
   private _ruleStore: SqliteRuleStore | undefined;
+  private _todoStore: SqliteTodoStore | undefined;
 
   constructor(private options: SqliteStorageProviderOptions) {}
 
@@ -26,6 +28,7 @@ export class SqliteStorageProvider implements StorageProvider {
       new SqliteSchemaManager(this.db).migrate();
       this._sessionStore = new SqliteSessionStore(this.db);
       this._ruleStore = new SqliteRuleStore(this.db);
+      this._todoStore = new SqliteTodoStore(this.db);
     } catch (err) {
       if (err instanceof StorageError) throw err;
       throw wrapSqliteError(
@@ -41,6 +44,7 @@ export class SqliteStorageProvider implements StorageProvider {
     this.db = undefined;
     this._sessionStore = undefined;
     this._ruleStore = undefined;
+    this._todoStore = undefined;
   }
 
   get sessionStore(): SqliteSessionStore {
@@ -51,5 +55,10 @@ export class SqliteStorageProvider implements StorageProvider {
   get ruleStore(): SqliteRuleStore {
     if (!this._ruleStore) throw new StorageError('DB_OPEN', 'StorageProvider not initialized');
     return this._ruleStore;
+  }
+
+  get todoStore(): SqliteTodoStore {
+    if (!this._todoStore) throw new StorageError('DB_OPEN', 'StorageProvider not initialized');
+    return this._todoStore;
   }
 }
